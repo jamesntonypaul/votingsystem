32524881c04a4893779f357b8a89061f22a36b3c
jamesantonypaul <110448939+jamesntonypaul@users.noreply.github.com>
2024-07-09 12:04:50 +0530

    Update config.js

--- BackEnd/config.js
+++ BackEnd/config.js
@@ -1,13 +1,13 @@
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwAHEJqCzxsjspVigf7OyfD_G3-RZW3uE",
-  authDomain: "voting-system-f4e07.firebaseapp.com",
-  databaseURL: "https://voting-system-f4e07-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "voting-system-f4e07",
-  storageBucket: "voting-system-f4e07.appspot.com",
-  messagingSenderId: "121285527313",
-  appId: "1:121285527313:web:bd0e0f426a94f37cfceeb0",
-  measurementId: "G-F4RNDSZ3QF"
+  apiKey: "----XXX----E",
+  authDomain: "----XXX----",
+  databaseURL: "----XXX----",
+  projectId: "----XXX----",
+  storageBucket: "----XXX----",
+  messagingSenderId: "----XXX----",
+  appId: "----XXX----",
+  measurementId: "----XXX----"
 };
 
 module.exports = firebaseConfig;
