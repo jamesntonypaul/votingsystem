79eb214071c12b25e1e4664d6bebc928af13de6e
jamesantonypaul <110448939+jamesntonypaul@users.noreply.github.com>
2024-07-09 12:17:45 +0530

    Update firebaseconfig.js

--- FrontEnd/firebaseconfig.js
+++ FrontEnd/firebaseconfig.js
@@ -9,14 +9,14 @@ import {getStorage} from "firebase/storage"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCwAHEJqCzxsjspVigf7OyfD_G3-RZW3uE",
-  authDomain: "voting-system-f4e07.firebaseapp.com",
-  databaseURL: "https://voting-system-f4e07-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "voting-system-f4e07",
-  storageBucket: "voting-system-f4e07.appspot.com",
-  messagingSenderId: "121285527313",
-  appId: "1:121285527313:web:bd0e0f426a94f37cfceeb0",
-  measurementId: "G-F4RNDSZ3QF"
+  apiKey: "----XXXX----",
+  authDomain: "----XXXX----",
+  databaseURL: "----XXXX----",
+  projectId: "----XXXX----",
+  storageBucket: "----XXXX----",
+  messagingSenderId: "----XXXX----",
+  appId: "----XXXX----",
+  measurementId: "----XXXX----"
 };
 
 // Initialize Firebase
